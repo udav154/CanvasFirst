29d736f6fb6e47ba432021e1558a7e95d021b15b
mos_ef <zaxarov_p@garpix.com>
2022-07-20 11:06:12 +0300

    fix path bug

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import Canvas from "./Canvas";
 import SettingsSide from "./SettingsSide";
 import AsyncComponent from "./AsyncComponent";
 import Viewport from "./Viewport";
-import TrailCursor from "./TrailCirsor ";
+import TrailCursor from "./TrailCursor";
 import CanvasUI from "./CanvasUI";
 import CursorCircle from "./CursorCircle";
 import Stats from "./Stats";
